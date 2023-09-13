769b34b2c229fe7413b13a62032ceb9c76dd3872
islam sani <islammuhd8@gmail.com>
2023-09-13 10:07:38 +0100

    mapping through array in react

---
+++ src/Task.jsx
@@ -0,0 +1,24 @@
+const Task = () => {
+    const details = [
+        {name :'Hassan', phoneNo : '08123432345', email : 'hassan@gmail.com', id:1},
+        {name :'Ahmed', phoneNo : '08196745754', email : 'ahmed@gmail.com', id:2},
+        {name :'Islam', phoneNo : '08100050507', email : 'islam@gmail.com', id:3},
+        {name :'Bashir', phoneNo : '080877564532', email : 'bash@gmail.com', id:4},
+        {name :'Umar', phoneNo : '09044553234', email : 'umar@gmail.com', id:5}
+    ]
+
+    return ( 
+        <>
+            <h1>The Details</h1>
+            {details.map(person => (
+                <div className="info" key={details.id}>
+                    <p>{person.name}: {person.phoneNo},  {person.email}</p>
+                    {/* <p>{person.phoneNo}</p> */}
+                    {/* <p>{person.email}</p> */}
+                </div>
+            ))}
+        </>
+     );
+}
+ 
+export default Task;
